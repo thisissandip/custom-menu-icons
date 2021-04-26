38fff9a79018079c22509aa0e62dc74e22c20bfa
thisissandip <thisissandipmondal@gmail.com>
2021-04-26 15:37:23 +0530

    Menu Icons Plugin Done

---
+++ custom-menu-icons.php
@@ -0,0 +1,329 @@
+<?php
+/**
+ * Custom Menu Icons
+ *
+ * @package           PluginPackage
+ * @author            Your Name
+ * @copyright         2021 SandipMondal
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Menu Icons
+ * Plugin URI:        
+ * Description:       Add Custom Icons to Your Menu EASILY.
+ * Version:           1.0.0
+ * Author:            Sandip Mondal
+ * Author URI:        
+ * Text Domain:       menu-icons
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if(!defined("ABSPATH")) : exit(); endif;
+
+
+// Activation and Deactivation Hooks
+
+register_activation_hook( __FILE__, "activate_menu_icons" );
+
+function activate_custom_menu_icons(){
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook( __FILE__, "deactivate_menu_icons" );
+
+function deactivate_custom_menu_icons(){
+    flush_rewrite_rules();
+}
+
+
+// Define Constants
+
+define("MI_PATH", trailingslashit( plugin_dir_path(__FILE__) ));
+define("MI_URL", trailingslashit( plugins_url("/", __FILE__) ));
+
+
+// Enqueue Admin Scripts and Styles
+
+add_action( "admin_enqueue_scripts", "enqueue_admin_menu_icons_scripts" );
+
+function enqueue_admin_menu_icons_scripts($hook){
+
+    if($hook !== "nav-menus.php"){
+        return;
+    }
+    wp_enqueue_style( "admin-menuicon-materialicons", "https://fonts.googleapis.com/icon?family=Material+Icons" );
+    wp_enqueue_style( "admin-menuicon-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" );
+    wp_enqueue_style( "menuiconadminstyles", MI_URL."css/menustyles.css", array("admin-menuicon-materialicons","admin-menuicon-fontawesome"), 1.0);
+
+    wp_enqueue_script("jquery");
+    wp_enqueue_script( "menuiconsalliconscript", MI_URL."js/allicons.js", array(), 1.0, true );
+    wp_enqueue_script( "menuiconsadminscript", MI_URL."js/menuiconscript.js", array("menuiconsalliconscript", "jquery"), 1.0, true );
+
+}
+
+
+// Enqueue Front End Scripts and Styles
+
+add_action( "wp_enqueue_scripts", "enqueue_frontend_menu_icons_scripts" );
+
+function enqueue_frontend_menu_icons_scripts(){
+    wp_enqueue_style( "admin-menuicon-materialicons", "https://fonts.googleapis.com/icon?family=Material+Icons" );
+    wp_enqueue_style( "front-menuicon-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" );
+    wp_enqueue_style( "menuiconfrontendstyles", MI_URL."css/menufrontendstyles.css", array(), 1.0);
+}
+
+
+// Adding Custom Menu Fields
+
+
+add_action( 'wp_nav_menu_item_custom_fields', 'menu_icons_fields', 10, 2 );
+
+function menu_icons_fields($item_id, $item){
+    ?>
+   <?php
+        wp_nonce_field( "mi_nonce", "_mi_meta_nonce_$item_id" );
+        // meta key for the custom field
+        // All the inputs [icon_library, icon_name,font_size,vertical_alignment] are saved in one meta key as an array
+        
+        $menuiconInfo = get_post_meta( $item_id, "_menu_icon_icon_name", true );
+
+        $icon_library = $menuiconInfo[0];
+        $icon_name = $menuiconInfo[1];
+        $icon_font_size = $menuiconInfo[2];
+        $icon_ver_align = $menuiconInfo[3];
+
+
+        switch($icon_library){
+            case 'font-awesome-icon':
+                $menuIconHTML = '<i class="font-awesome-icon '.$icon_name.'"></i>';
+                break;
+            case 'material-icons':
+                $menuIconHTML = '<span class="material-icons material-icon-size" >'.$icon_name.'</span> ';
+                break;
+            case 'dashicons':
+                $menuIconHTML = '<i class="dashicons '.$icon_name.'"></i>';
+                break;
+            default:
+                break;
+        }
+       
+    ?>
+
+    <!-- Custom Menu Fields Start -->
+        
+    <div class="menu-icon-field-wrapper">
+   
+            <div class="menu-icon-field-conatiner menu-icon-field-container-<?php echo $item_id ?>">
+                    <input type="hidden" name="_menu_icon_icon_name[<?php echo $item_id; ?>]"
+                    id="_menu_icon_icon_name-<?php echo $item_id; ?>" value= "<?php echo isset($icon_name) ? $icon_name : ""; ?>" >
+
+                    <input type="hidden" name="_menu_icon_icon_library[<?php echo $item_id; ?>]"
+                    id="_menu_icon_icon_library-<?php echo $item_id; ?>" value= "<?php echo isset($icon_library) ? $icon_library : ""; ?>" >
+
+                    <button class="menu-icon-start-btn" 
+                    data-itemid = "<?php echo $item_id ?>"
+                    >
+                        <?php echo _e("Select Icon", 'menu-icons') ?> 
+                    </button> 
+
+                    <div class="icon-preview icon-preview-<?php echo $item_id ?>"> 
+                        <?php echo $menuIconHTML ?>
+                    </div>
+
+
+            </div>
+
+            <div class="menu-icons-details-input-container">
+
+                        <div class="font-input-container">
+                            <label for="_menu_icon_font_size-<?php echo $item_id ;?>">
+                                <?php _e( 'Icon Size (pixels)', 'menu-icons'); ?>
+                            </label>
+
+                            <input type="number" id="_menu_icon_font_size-<?php echo $item_id ;?>" class="menu-icon-size-input" step=".1" 
+                            name="_menu_icon_icon_name[font_size][<?php echo $item_id ;?>]" value="<?php echo isset($icon_font_size) && $icon_font_size !=="" ? $icon_font_size : 22; ?>" />
+                        </div>
+
+                        <div class="font-input-container">
+                            <label for="_menu_icon_font_size-<?php echo $item_id ;?>">
+                                <?php _e( 'Icon Vertical Alignment', 'menu-icons'); ?>
+                            </label>
+
+                            <?php echo isset($icon_ver_align) && $icon_ver_align == "top" ? "selected" : "" ;?>
+
+                            <select class="menu-icon-select-field" name="_menu_icon_icon_name[vertical_align][<?php echo $item_id; ?>]" id="_menu_icon_vertical_align-<?php echo $item_id ;?>">
+                                
+                                <option value="top" 
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "top" ? "selected" : "" ;?> 
+                                >Top</option>
+
+                                <option value="bottom"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "bottom" ? "selected" : "" ;?>
+                                >Bottom</option>
+
+                                <option value="middle"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "middle" ? "selected" : "" ;?>
+                                >Middle</option>
+
+                                <option value="sub"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "sub" ? "selected" : "" ;?>
+                                >Sub</option>
+
+                                <option value="text-top"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "text-top" ? "selected" : "" ;?>
+                                >Text-Top</option>
+
+                                <option value="text-bottom"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "text-bottom" ? "selected" : "" ;?>
+                                >Text-Bottom</option>
+
+                                <option value="super"
+                                    <?php echo isset($icon_ver_align) && $icon_ver_align == "super" ? "selected" : "" ;?>
+                                >Super</option>
+
+                            </select>
+                        </div>
+
+            </div>
+    </div>
+  <!-- Custom Menu Fields Ends Here-->
+
+    <?php
+}
+
+
+// Adding Menu Icon Modal in the Footer
+
+add_action("admin_footer-nav-menus.php", "insert_menu_icons_modal",10,2);
+
+function insert_menu_icons_modal(){
+    ?>
+    <!-- Modal Starts Here-->
+    <div class="menu-icons-modal-wrapper">
+        <div class="menu-icons-modal-container">
+
+            <!-- Left Container Starts Here-->
+            <div class="left-menu-icon-container">
+                    <div class="filter-library-container">
+                        <div class="menu-icon-logo"> MENU ICONS</div>
+                        <button id="filter-tab-btn" class="is-active" data-library="all-icons" ><i class="fas fa-hashtag button-icon"></i> All Icons</button>
+                        <button id="filter-tab-btn" data-library="dash-icon" ><i class="fab fa-wordpress-simple button-icon"></i> Dashicons</button>
+                        <button id="filter-tab-btn" data-library="font-awesome" ><i class="fab fa-font-awesome-flag button-icon"></i>  Font Awesome</button>
+                        <button id="filter-tab-btn" data-library="material-icon" ><i class="fab fa-google button-icon"></i> Material Icons</button>
+                        
+                    </div>
+            </div>
+            <!-- Left Container Ends Here-->
+
+            <!-- Right Container Starts Here-->
+            <div class="right-menu-icon-container">
+                <div class="search-container">
+                    
+                    <div class="search-bar-input-container">
+                        <input id="search-bar-input-field" type="text" placeholder="Search Icons" >
+                        <i class="fas fa-search search-icon-input"></i>
+                    </div>
+                            
+                    <button class="close-modal-btn"><i class="fas fa-times"></i></button>
+                </div>
+
+                <div class="all-menu-icons-wrapper">
+                    <div class="all-menu-icons-container font-awesome-container"></div>
+                    <div class="all-menu-icons-container dashicon-container"></div>
+                    <div class="all-menu-icons-container material-icon-container"></div>
+                    <div class="all-menu-icons-container search-result-icon-container"></div>
+                </div>
+            </div>
+             <!-- Right Container Ends Here-->
+        </div>
+    </div>
+
+  <!-- Modal Ends Here-->
+    <?php
+}
+
+
+// Update the Custom Field on Save Menu
+
+add_action( 'wp_update_nav_menu_item', 'update_menu_icons', 10, 2 );
+
+function update_menu_icons($menu_id, $menu_item_db_id){
+
+     // Verify this came from our screen and with proper authorization.
+	if ( ! isset( $_POST["_mi_meta_nonce_$menu_item_db_id"] ) || ! wp_verify_nonce( $_POST["_mi_meta_nonce_$menu_item_db_id"], 'mi_nonce' ) ) {
+        return $menu_id;
+	} 
+
+    // Check if the fields are set, If they are sanitize the inputs and update the meta of that nav menu item
+    if ( isset( $_POST['_menu_icon_icon_name'][$menu_item_db_id]) &&
+        isset($_POST['_menu_icon_icon_name']['vertical_align'][$menu_item_db_id]) &&
+        isset($_POST['_menu_icon_icon_name']['font_size'][$menu_item_db_id]) &&
+        isset($_POST['_menu_icon_icon_library'][$menu_item_db_id])
+    )  {
+        $icon_lib = sanitize_text_field($_POST['_menu_icon_icon_library'][$menu_item_db_id]) ;
+        $icon_name = sanitize_text_field($_POST['_menu_icon_icon_name'][$menu_item_db_id]) ;
+        $font_size = sanitize_text_field($_POST['_menu_icon_icon_name']['font_size'][$menu_item_db_id]);
+        $vertical_align = sanitize_text_field($_POST['_menu_icon_icon_name']['vertical_align'][$menu_item_db_id]);
+        
+        // save data in array [ icon_library, icon_name, icon_font_size_in_px, icon_vertical_align ]
+		$sanitized_data = array($icon_lib,$icon_name,$font_size,$vertical_align);
+		update_post_meta( $menu_item_db_id, '_menu_icon_icon_name', $sanitized_data );
+	} 
+
+}
+
+
+// Show the Menu Icon In Title (Website)
+
+add_filter("nav_menu_item_title", "admin_menu_icon_display", 10,2); 
+
+function admin_menu_icon_display($title, $item){
+
+    // get the meta data of the nav_menu_item 
+    if( is_object( $item ) && isset( $item->ID ) ) {
+        $menuiconInfo = get_post_meta( $item->ID, "_menu_icon_icon_name", true );
+
+        $icon_library = $menuiconInfo[0];
+        $icon_name = $menuiconInfo[1];
+        $icon_font_size = $menuiconInfo[2];
+        $icon_ver_align = $menuiconInfo[3];
+
+        // according to the library filter the Output HTML
+        if($icon_library){
+            switch($icon_library){
+                case 'font-awesome-icon':
+                    $menuIconHTML = '<i class="menu-icon '.$icon_name.'" 
+                                    style=" font-size: '.$icon_font_size.'px; 
+                                    vertical-align: '.$icon_ver_align.'" ></i>';
+                    break;
+                case 'material-icons':
+                    $menuIconHTML = ' <i class="material-icons material-icon-size menu-icon"
+                                    style=" font-size: '.$icon_font_size.'px; 
+                                    vertical-align: '.$icon_ver_align.'"
+                                    >'.$icon_name.'</i> ';
+                    break;
+                case 'dashicons':
+                    $menuIconHTML = '<i class="dashicons '.$icon_name.' menu-icon" 
+                                    style=" font-size: '.$icon_font_size.'px; 
+                                    vertical-align: '.$icon_ver_align.'"
+                                    ></i>';
+                    break;
+                default:
+                    break;
+            } 
+        }
+
+        // if Icon is already selected then return new Title (which is title with icon)
+        // else return the old title
+        if(! empty($menuIconHTML)){
+            $newtitle = ''.$menuIconHTML.' '.$title.'';
+            return $newtitle;
+        }else{
+            return $title;
+        }
+    }
+
+}
+
+?>
---
+++ js/menuiconscript.js
@@ -0,0 +1,254 @@
+jQuery(document).ready(function ($) {
+	let selectIconBtn = document.querySelectorAll('.menu-icon-start-btn');
+	let modal = document.querySelector('.menu-icons-modal-wrapper');
+	let close = document.querySelector('.close-modal-btn');
+
+	let allIconWrapper = document.querySelector('.all-menu-icons-wrapper');
+	let allIconCont = document.querySelectorAll('.all-menu-icons-container');
+	let FACont = document.querySelector('.font-awesome-container');
+	let MATCont = document.querySelector('.material-icon-container');
+	let DASHCont = document.querySelector('.dashicon-container');
+	let SEARCHCont = document.querySelector('.search-result-icon-container');
+	let filterTabBtns = document.querySelectorAll('#filter-tab-btn');
+
+	let searchInput = document.querySelector('#search-bar-input-field');
+
+	let ITEM_ID = null;
+	let ITEM_ICON_NAME = null;
+
+	// SET selected icon library default to Font Awesome
+	let SELECTED_ICON_LIBRARY = 'ALL-ICONS';
+
+	// Map all Font Awesome Icons
+	let allfontawesomeIcons = allFAicons
+		.map((icon_class) => {
+			let trimmedIconname = icon_class.replace(/\s/g, '');
+			return `<div class="select-icon-container icon-container-${trimmedIconname}" 
+					data-iconname="${icon_class}"> 
+					<i class='font-awesome-icon ${icon_class}'></i>
+				</div>`;
+		})
+		.join('');
+	FACont.innerHTML = `${allfontawesomeIcons}`;
+
+	// Map all Material Icons and Load the Fonts
+	let allmaterialIcons = allMaticons
+		.map((icon_name) => {
+			let trimmedIconname = icon_name.replace(/\s/g, '');
+			return `<div class="select-icon-container icon-container-${trimmedIconname}"
+					data-iconname="material ${icon_name}"
+					> 
+				<i class='material-icons'>${icon_name}</i>
+			</div>`;
+		})
+		.join('');
+	MATCont.innerHTML = `${allmaterialIcons}`;
+
+	// Map all DashIcons and Load the Fonts
+	let allDashIcons = allDash
+		.map((icon_class) => {
+			let trimmedIconname = icon_class.replace(/\s/g, '');
+			return `<div class="select-icon-container icon-container-${trimmedIconname}"
+					data-iconname="${icon_class}"
+					> 
+			<span class="dashicons ${icon_class}"></span>
+		</div>`;
+		})
+		.join('');
+	DASHCont.innerHTML = `${allDashIcons}`;
+
+	// Display Modal On Click of Select Icon Button and Save the ITEM_ID of the Nav Item
+	selectIconBtn.forEach((btn) => {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			ITEM_ID = btn.getAttribute('data-itemid');
+			modal.classList.add('is-active');
+
+			// For the Nav Menu Items that already have a Icon Selected. Display their Selected Icon in Modal
+			let menuItemMetaIconName = document.querySelector(
+				`#_menu_icon_icon_name-${ITEM_ID}`
+			).value;
+			let menuItemMetaIconLib = document.querySelector(
+				`#_menu_icon_icon_library-${ITEM_ID}`
+			).value;
+
+			if (menuItemMetaIconName && menuItemMetaIconLib) {
+				let trimmedMetaIcon = menuItemMetaIconName.replace(/\s/g, '');
+				let selectIconContainer = document.querySelector(
+					`.icon-container-${trimmedMetaIcon}`
+				);
+				console.log(trimmedMetaIcon);
+				selectIconContainer.classList.add('selected');
+			}
+		});
+	});
+
+	// On Selection of the Item, Get the classname of Icon and add selected class and update the hidden input values
+	let iconCont = document.querySelectorAll('.select-icon-container');
+	iconCont.forEach((icon) => {
+		icon.addEventListener('click', () => {
+			unselectAllIcons();
+			let iconName;
+			let iconClassname;
+			let iconLibrary;
+			let iconpreview = document.querySelector(`.icon-preview-${ITEM_ID}`);
+
+			let hiddenIconNameField = document.querySelector(
+				`#_menu_icon_icon_name-${ITEM_ID}`
+			);
+			let hiddenIconLibraryField = document.querySelector(
+				`#_menu_icon_icon_library-${ITEM_ID}`
+			);
+
+			let childClass = icon.childNodes[1].getAttribute('class');
+			let childClassArray = childClass.split(' ');
+			iconLibrary = childClassArray[0];
+
+			switch (iconLibrary) {
+				case 'font-awesome-icon':
+					// get classname of selected icon
+					iconClassname = icon
+						.querySelector('.font-awesome-icon')
+						.getAttribute('class');
+					icon.classList.add('selected');
+					iconName = iconClassname.substr(18);
+					ITEM_ICON_NAME = iconName;
+					// change the preview icon
+					iconpreview.innerHTML = `<i class="${ITEM_ICON_NAME}"></i> `;
+					// update value of hidden field to the new classname
+					hiddenIconLibraryField.value = 'font-awesome-icon';
+					hiddenIconNameField.value = `${ITEM_ICON_NAME}`;
+					break;
+
+				case 'material-icons':
+					iconName = icon.querySelector('.material-icons').innerText;
+					icon.classList.add('selected');
+					ITEM_ICON_NAME = iconName;
+					// change the preview icon
+					iconpreview.innerHTML = `<i class="material-icons">${ITEM_ICON_NAME}</i> `;
+					// update value of hidden field to the new classname
+					hiddenIconLibraryField.value = 'material-icons';
+					hiddenIconNameField.value = `${ITEM_ICON_NAME}`;
+					break;
+
+				case 'dashicons':
+					iconClassname = icon
+						.querySelector('.dashicons')
+						.getAttribute('class');
+					icon.classList.add('selected');
+					iconName = iconClassname.substr(10);
+					ITEM_ICON_NAME = iconName;
+					// change the preview icon
+					iconpreview.innerHTML = `<i class="dashicons ${ITEM_ICON_NAME}"></i> `;
+					// update value of hidden field to the new classname
+					hiddenIconLibraryField.value = 'dashicons';
+					hiddenIconNameField.value = `${ITEM_ICON_NAME}`;
+					break;
+
+				default:
+					break;
+			}
+
+			console.log(iconName);
+		});
+	});
+
+	function unselectAllIcons() {
+		iconCont.forEach((item) => {
+			item.classList.remove('selected');
+		});
+	}
+
+	// filter Icon library
+	filterTabBtns.forEach((tab) => {
+		tab.addEventListener('click', () => {
+			let library = tab.getAttribute('data-library');
+			FilterIcons(library, tab);
+		});
+	});
+
+	function FilterIcons(library, tab) {
+		allIconCont.forEach((cont) => (cont.style.display = 'none'));
+		filterTabBtns.forEach((btn) => btn.classList.remove('is-active'));
+		allIconWrapper.scrollTop = 0;
+
+		switch (library) {
+			case 'font-awesome':
+				SELECTED_ICON_LIBRARY = 'FONT-AWESOME';
+				FACont.style.display = 'grid';
+				tab.classList.add('is-active');
+				break;
+			case 'material-icon':
+				SELECTED_ICON_LIBRARY = 'MATERIAL-ICON';
+				MATCont.style.display = 'grid';
+				tab.classList.add('is-active');
+				break;
+			case 'dash-icon':
+				SELECTED_ICON_LIBRARY = 'DASH-ICON';
+				DASHCont.style.display = 'grid';
+				tab.classList.add('is-active');
+				break;
+			case 'all-icons':
+				SELECTED_ICON_LIBRARY = 'ALL-ICON';
+				FACont.style.display = 'grid';
+				MATCont.style.display = 'grid';
+				DASHCont.style.display = 'grid';
+				tab.classList.add('is-active');
+			case 'searched':
+				SELECTED_ICON_LIBRARY = 'ALL-ICON';
+				SEARCHCont.style.display = 'grid';
+				tab.classList.add('is-active');
+			default:
+				break;
+		}
+	}
+
+	// Close the Modal
+	close.addEventListener('click', () => {
+		unselectAllIcons();
+		modal.classList.remove('is-active');
+		searchInput.value = '';
+
+		let tab = document.querySelector(`[data-library=all-icons]`);
+		setTimeout(() => {
+			FilterIcons('all-icons', tab);
+		}, 500);
+	});
+
+	// Search Bar Input Field
+	let allIcons = document.querySelectorAll('.select-icon-container');
+
+	// When Enter is Pressed get the searched input and filter Icons
+	searchInput.addEventListener('keyup', (e) => {
+		if (e.keyCode === 13) {
+			let searchTerm = searchInput.value;
+			let searchTermLowerCase = searchTerm.toLowerCase();
+
+			// Filter the icon containers according to the data tag
+			let searchRESULT = [...allIcons].filter((icon) => {
+				let thisiconname = icon.getAttribute('data-iconname');
+				let result = thisiconname.search(`${searchTermLowerCase}`);
+				if (result > 0) {
+					return thisiconname;
+				}
+			});
+			// Clear the Search Container
+			SEARCHCont.innerHTML = '';
+
+			// Map the searched Result and Fill into the Search Container
+			if (searchRESULT.length > 0) {
+				searchRESULT.forEach((result_icon) => {
+					SEARCHCont.appendChild(result_icon);
+				});
+			} else {
+				SEARCHCont.innerHTML =
+					'<div class="menu-icon-no-results">No Results</div>';
+			}
+
+			// Select All Icons Tab on search
+			let tab = document.querySelector(`[data-library=all-icons]`);
+			FilterIcons('searched', tab);
+		}
+	});
+});
